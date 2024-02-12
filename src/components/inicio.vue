0fbf5de58bc0ae778dbd4647efbebdf1f871d659
RubenNH <104938082+RubenNH@users.noreply.github.com>
2024-02-11 19:53:49 -0600

    Adaptacion para recibir generos y reenviarlos al back

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -17,7 +17,12 @@
                         placeholder="Ingresa el año de la película"></b-form-input>
 
                     <b-form-group id="input-group-4" label="Género" label-for="input-3">
-                        <b-form-select id="input-3" v-model="form.genre" :options="genres" required></b-form-select>
+                        <b-form-select v-model="form.genre" :options="genres" required>
+                            <template v-slot:first>
+                                <option :value="0">Selecciona un género</option>
+                            </template>
+                        </b-form-select>
+                        <div v-if="genres.length === 0">Loading genres...</div>
                     </b-form-group>
 
                     <b-button type="submit" variant="primary">Registrar</b-button>
@@ -43,24 +48,26 @@ export default {
                 year: ''
             },
             genres: [],
+            genresToSend: [],
             showModal: false
         };
     },
     mounted() {
         // Llamar a la API para obtener los géneros disponibles
         this.fetchGenres();
+        //set default value 0
+        this.form.genre = 0;
+
     },
     methods: {
         openModal() {
             this.showModal = true;
         },
         onSubmit(evt) {
-            evt.preventDefault();
             // Enviar la película a la API
             this.registerMovie();
         },
         onReset(evt) {
-            evt.preventDefault();
             this.form.name = '';
             this.form.genre = '';
             this.form.year = '';
@@ -69,13 +76,24 @@ export default {
         async fetchGenres() {
             try {
                 const response = await axios.get('http://localhost:8080/api/movies/genres');
-                this.genres = response.data;
+
+                this.genres = response.data.map(genre => ({ text: genre.name, value: genre.id }));
+                this.genresToSend = response.data;
             } catch (error) {
                 console.error('Error al obtener los géneros:', error);
+                this.errorFetchingGenres = 'Error al obtener los géneros. Por favor, inténtelo de nuevo.';
             }
         },
         async registerMovie() {
             try {
+                if (this.form.genre === 0) {
+                    alert('Por favor, selecciona un género.');
+                    return;
+                }
+                //buscar el id del genero y enviar el objeto completo
+                this.form.genre = this.genresToSend.find(genre => genre.id === this.form.genre);
+                console.log(this.form.genre);
+
                 await axios.post('http://localhost:8080/api/movies/movies', this.form);
                 alert('¡Película registrada con éxito!');
                 this.onReset(); // Limpiar el formulario después de registrar la película
